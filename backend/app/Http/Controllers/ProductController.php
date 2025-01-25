6d50959d581d9234ae2369938e8dbc30f4896889
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2025-01-26 00:04:05 +0600

    basic api develop for products

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
-    //
+    // Fetch all products
+    public function index()
+    {
+        $products = Product::query()
+                    ->latest()
+                    ->get();
+
+        return response()->json([
+            'data' => $products,
+            'message' => 'Data fetch successfully!',
+        ], Response::HTTP_OK);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'data' => [],
+                'message' => 'Product not foun!',
+            ], Response::HTTP_NOT_FOUND);
+        }
+        return response()->json([
+            'data' => $product,
+            'message' => 'Data fetch successfully!',
+        ], Response::HTTP_OK);
+    }
 }
